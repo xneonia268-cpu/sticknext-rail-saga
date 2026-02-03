0456185521947f03c35df9d827907e262ac81bdf
xneonia268-cpu <xneonia268@gmail.com>
2026-02-03 08:17:29 +0700

    Add files via upload

--- script.js
+++ script.js
@@ -1,17 +1,19 @@
-// Jam Digital + Tanggal
 function updateClock() {
   const now = new Date();
   const days = ["Minggu","Senin","Selasa","Rabu","Kamis","Jumat","Sabtu"];
+  const months = ["Januari","Februari","Maret","April","Mei","Juni","Juli","Agustus","September","Oktober","November","Desember"];
+
   const dayName = days[now.getDay()];
-  const date = String(now.getDate()).padStart(2, "0");
-  const month = String(now.getMonth() + 1).padStart(2, "0");
+  const date = now.getDate();
+  const monthName = months[now.getMonth()];
   const year = now.getFullYear();
+
   const hours = String(now.getHours()).padStart(2, "0");
   const minutes = String(now.getMinutes()).padStart(2, "0");
   const seconds = String(now.getSeconds()).padStart(2, "0");
 
   document.getElementById("clock").textContent =
-    `${dayName}, ${date}/${month}/${year} - ${hours}:${minutes}:${seconds}`;
+    `${dayName}, ${date} ${monthName} ${year} | ${hours}:${minutes}:${seconds}`;
 }
 
 setInterval(updateClock, 1000);
